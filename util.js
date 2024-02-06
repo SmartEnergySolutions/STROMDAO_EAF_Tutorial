7c2b45e7635f79186a395995bb12fc2fa3f3afff
Thorsten Zoerner <me@thorsten-zoerner.com>
2024-02-06 14:47:44 +0100

    auto

--- util.js
+++ util.js
@@ -3,7 +3,7 @@ module.exports = {
     openUrl:function(url) {
         if(typeof process.env.GITPOD_HOST !== 'undefined') {
             const { exec } = require('child_process'); // Required to open Previews in GitPod Environment
-            exec('gp preview '+url);
+            exec('gp preview "'+url+'"');
         } else {
             const start = (process.platform == 'darwin'? 'open': process.platform == 'win32'? 'start': 'xdg-open');
             require('child_process').exec(start + ' ' + url);
