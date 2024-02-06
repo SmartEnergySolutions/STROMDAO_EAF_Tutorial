6ab84ccc9e6ee31b403bdb5001b2f7e5e2b32f21
Thorsten Zoerner <me@thorsten-zoerner.com>
2024-02-06 12:12:53 +0100

    auto

---
+++ 1_updateReading.js
@@ -0,0 +1,16 @@
+const EAF = require("stromdao-eaf");
+const { exec } = require('child_process'); // Required to open Previews in GitPod Environment
+
+const app = async function() {
+    // Generate first meter reading
+    const first_meter_reading = Math.round(Math.random()*10000);
+
+    // Create EAF Node and call metering.updateReading
+    const eaf_node = await EAF.node();
+    const result = await eaf_node.call("metering.updateReading",{time:new Date().getTime(),meterId:"tutorial1",reading:first_meter_reading});
+    
+    // Open Preview in Gitpod
+    exec('gp preview http://localhost:3000/api/readings_model/');
+}
+
+app();
